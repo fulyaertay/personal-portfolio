70a5aee2c52d8c878b435a47f27235c6e2c2181d
Fulya <fulya.ertay@gmail.com>
2023-01-05 00:24:44 +0300

    added resume link

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl"></BsFillMoonStarsFill>
               </li>
               <li>
-                <a  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 roundedn-md ml-8"href="#">Resume</a>
+                <a  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 roundedn-md ml-8"href="https://www.showwcase.com/fulyaertay">Resume</a>
               </li>
             </ul>
           </nav>
