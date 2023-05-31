42576acfaf87578cc132d52a4c22f4dc2927ea79
Fulya <fulya.ertay@gmail.com>
2023-05-31 13:17:53 +0300

    bg-gradient-to-r added

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home() {
               </li>
               <li className="hidden lg:flex md:flex">
                 <a
-                  className="bg-gradient-to-r from-cyan-500 tracking-widest hover:to-teal-400  to-teal-800 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500  hover:to-teal-400  to-teal-800 text-white px-4 py-2 rounded-md ml-8"
                   href="https://www.showwcase.com/fulyaertay"
                 >
                   Resume
