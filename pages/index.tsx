d2408f02a95f4359d8789f7734b6870c74cdfdce
Fulya <fulya.ertay@gmail.com>
2023-06-08 19:44:56 +0300

    link font sizes updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,18 +35,18 @@ export default function Home() {
             </h1>
 
             <ul className="flex items-center gap-10">
-              <li className="text-xl  text-white ">
+              <li className="text-lg  text-white ">
                 <a href="#projects" id="projects-link">Projects </a>
               </li>
-              <li className="text-xl  text-white ">
+              <li className="text-lg  text-white ">
                 <a id="about-link" href="#about">About </a>
               </li>
-              <li className="text-xl text-white ">
+              <li className="text-lg text-white ">
                 <a id="contact-link" href="#contact">Contact </a>
               </li>
               <li className="hidden lg:flex md:flex">
                 <a id="resume-link"
-                  className="bg-gradient-to-r from-cyan-500  hover:to-teal-400  to-teal-800 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r text-lg from-cyan-500  hover:to-teal-400  to-teal-800 text-white px-4 py-2 rounded-md ml-8"
                   href="https://www.showwcase.com/fulyaertay"
                   target="_blank"
                   rel="noopener noreferrer"
