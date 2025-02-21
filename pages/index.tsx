7e1a8971f3cba799848d392d08450d4cb575cbf6
fulya <fulya.ertay@gmail.com>
2025-02-21 13:48:55 +0300

    doc validator project added

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ import web2 from "../public/web2.png";
 import web5 from "../public/web7.png";
 import web7 from "../public/web9.png";
 import falcowms from "../public/falcowms.png";
-import patikastore from "../public/patikastore.png";
+import doc_validator from "../public/doc_validator.png";
 import bookApplication from "../public/book-homepage.png"
 import eylul from "../public/9eylul.png"
 import djavac from "../public/djavac.png"
@@ -181,7 +181,57 @@ export default function Home() {
           </div>
           <div className="flex flex-col  ">
             <div className="grid grid-cols-1 md:grid-cols-2 mb-10 gap-4 ">
-             
+            <div>
+                {" "}
+                <Image
+                  alt="Document Validator"
+                  src={doc_validator}
+                  width={550}
+                  height={250}
+                ></Image>
+              </div>
+              <div>
+                <p className="font-md  text-gray-900 text-2xl">Document Validator App with Mistral AI </p>
+                <p className="font-md mt-7 text-gray-900">
+                A React app which allows users to directly input a Google Docs URL for document analysis, extracting its content and analyzing it. 
+                </p>
+                <div className="text-left flex items-center gap-8">
+                <a
+                    id="github"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/fulyaertay/document_validator"
+                    className="text-gray-900 text-5xl pt-4 pb-7 hover:text-slate-400"
+                  >
+                    <AiFillGithub></AiFillGithub>
+                  </a> <a
+                    className="tracking-widest    text-gray-900 pt-4 pb-7 text-left   inline-block  md:h-14  "
+                    href="https://document-validator-ai.vercel.app/"
+                  >
+                    <span className="inline-block underline hover:text-gray-600">
+                      Visit Site{" "}
+                      <svg
+                        className="inline-block"
+                        width="30"
+                        height="10"
+                        viewBox="0 0 16 9"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill="black"
+                          d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5Z"
+                        />
+                        <path
+                          fill="black"
+                          d="M10 8.5a.47.47 0 0 1-.35-.15c-.2-.2-.2-.51 0-.71l3.15-3.15l-3.15-3.15c-.2-.2-.2-.51 0-.71c.2-.2.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71l-3.5 3.5c-.1.1-.23.15-.35.15Z"
+                        />
+                      </svg>{" "}
+                    </span>
+                  </a>
+
+                
+                </div>
+              </div>
             <div>
                 {" "}
                 <Image
@@ -210,34 +260,7 @@ export default function Home() {
                 
                 </div>
               </div>
-              <div>
-                {" "}
-                <Image
-                  alt="PatikaStore"
-                  src={patikastore}
-                  width={550}
-                  height={250}
-                ></Image>
-              </div>
-              <div>
-                <p className="font-md  text-gray-900 text-2xl">PatikaStore Mobile App</p>
-                <p className="font-md mt-7 text-gray-900">
-                  A React Native project that provides to list product using FlatList component.
-                </p>
-                <div className="text-left flex items-center gap-8">
-                <a
-                    id="github"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/fulyaertay/PATIKASTORE-Mobile-App-?tab=readme-ov-file"
-                    className="text-gray-900 text-5xl pt-4 pb-7 hover:text-slate-400"
-                  >
-                    <AiFillGithub></AiFillGithub>
-                  </a> 
-
-                
-                </div>
-              </div>
+           
              
              
               <div>
