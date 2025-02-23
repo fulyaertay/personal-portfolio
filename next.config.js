036befc9603a7e5a80d1ebf6a4812cd645ac4658
fulya <fulya.ertay@gmail.com>
2025-02-23 21:10:11 +0300

    /** @type {import('next').NextConfig} */ const nextConfig = {   reactStrictMode: true,   env: {     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,   }, }
    module.exports = nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 }
 
 module.exports = nextConfig
